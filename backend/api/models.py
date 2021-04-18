75d7a7071ba39cfac4e1cc5078f9d5be156b81a0
adiab4 <adiab@luc.edu>
2021-04-18 15:50:38 -0500

    Added database and models for Projects, documents, and users

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,101 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.base_user import BaseUserManager
 from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser
+)
+
 
 # Create your models here.
+class document(models.Model):
+    doc_name = models.CharField(max_length=20)
+
+
+class Project(models.Model):
+    project_name = models.CharField(max_length=40)
+    from_dateRange = models.DateField()
+    to_dateRange = models.DateField()
+    tags = models.CharField(max_length=10)
+    documents = models.ManyToManyField(document)
+
+    def public_proj(self):
+        return super(Project, self).filter(private=False)
+
+    def private_proj(self):
+        user = get_user_model()
+        users = user.objects.all()
+        return super(Project, self).filter(private=True, user=users)
+
+
+class User(AbstractBaseUser):
+    username = models.CharField(
+        verbose_name='Username',
+        max_length=255,
+        unique=True,
+    )
+    is_active = models.BooleanField(default=True)
+    admin = models.BooleanField(default=False)  # a superuser
+
+    # notice the absence of a "Password field", that is built in.
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []  # required username and password fields
+
+    def get_full_name(self):
+        # The user is identified by username
+        return self.username
+
+    def get_short_name(self):
+        # The user is identified by their email address
+        return self.username
+
+    def __str__(self):
+        return self.username
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_admin(self):
+        "Is the user a admin member?"
+        return self.admin
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, password=None):
+        """
+                Creates and saves a User with the given email and password.
+                """
+        if not username:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_username(username),
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, password):
+        """
+        Creates and saves a superuser with the given email and password.
+        """
+        user = self.create_user(
+            username,
+            password=password,
+        )
+        user.admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    objects = UserManager()
