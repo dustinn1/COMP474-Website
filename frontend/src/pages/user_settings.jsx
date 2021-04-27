9b67cbd1cc6ecc4c32d1958bd569a53429d413e0
adiab4 <adiab@luc.edu>
2021-04-26 19:20:50 -0500

    More UI changes

--- frontend/src/pages/user_settings.jsx
+++ frontend/src/pages/user_settings.jsx
@@ -3,12 +3,29 @@ import { Helmet } from 'react-helmet';
 import Container from 'react-bootstrap/Container';
 
 import Navigation from '../components/navigation'
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Form from "react-bootstrap/Form";
+import {FormControl} from "react-bootstrap";
+import Button from "react-bootstrap/Button";
 
 
 export default function UserSettings() {
 
   return (
     <>
+        <Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="#home">Project Manager</Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Projects</Nav.Link>
+      <Nav.Link href="#pricing">Chat</Nav.Link>
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-info">Search</Button>
+    </Form>
+  </Navbar>
       <Helmet>
         <title>User Settings</title>
       </Helmet>
